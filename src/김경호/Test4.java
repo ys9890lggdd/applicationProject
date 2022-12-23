783e2f24b8d81c2c76e18586b60e8ef36d8846a5
academyitwill <academyitwill@gmail.com>
2022-12-23 17:29:16 +0900

    ..

---
+++ src/김경호/Test4.java
@@ -0,0 +1,32 @@
+package 김경호;
+
+public class Test4 {
+	private int no;
+	private String name;
+	public Test4() {
+		// TODO Auto-generated constructor stub
+	}
+	public Test4(int no, String name) {
+		super();
+		this.no = no;
+		this.name = name;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Test [no=" + no + ", name=" + name + "]";
+	}
+	
+
+}
