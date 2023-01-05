50528d478d2c5e733d5cd747b66478f3cc75a7d8
yuuuuna0 <jyn9306@gmail.com>
2023-01-05 16:00:12 +0900

    ..

--- src/김경호/TestMain.java
+++ src/김경호/TestMain.java
@@ -4,12 +4,11 @@ public class TestMain {
 
 	public static void main(String[] args) {
 
-		// TODO Auto-generated method stub
-//123
+
 		System.out.println("충돌");
 
 
-		// TODO Auto-generated method stub ddddsfa
+
 
 		System.out.println("test1강사");
 		System.out.println("야호");
