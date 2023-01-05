610367cc6e3d82fa1a5f039401f9229a1176f73d
alterack <kimjun030519@gmail.com>
2023-01-05 16:05:32 +0900

    ..

--- src/김경호/TestMain.java
+++ src/김경호/TestMain.java
@@ -6,13 +6,13 @@ public class TestMain {
 		System.out.println("충돌");
 
 		System.out.println("test1강사");
-<<<<<<< HEAD
+//<<<<<<< HEAD
 		System.out.println("test장장사진");
-=======
+//=======
 		System.out.println("test2강사");
 		System.out.println("야호");
 		System.out.println("깃");
->>>>>>> branch 'master' of https://github.com/2022-11-JAVA-DEVELOPER/applicationProject.git
+//>>>>>>> branch 'master' of https://github.com/2022-11-JAVA-DEVELOPER/applicationProject.git
 	}
 
 }
