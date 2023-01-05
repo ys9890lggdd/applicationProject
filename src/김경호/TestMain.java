6e6ecbb94f6c8cd026416f50e6ea7afffad44a53
academyitwill <academyitwill@gmail.com>
2023-01-05 16:04:06 +0900

    Merge branch 'master' of https://github.com/2022-11-JAVA-DEVELOPER/applicationProject.git

--- src/김경호/TestMain.java
+++ src/김경호/TestMain.java
@@ -6,13 +6,12 @@ public class TestMain {
 		System.out.println("충돌");
 
 		System.out.println("test1강사");
-<<<<<<< HEAD
+
 		System.out.println("test장장사진진");
-=======
+
 		System.out.println("test2강사");
 		System.out.println("야호");
 		System.out.println("깃");
->>>>>>> branch 'master' of https://github.com/2022-11-JAVA-DEVELOPER/applicationProject.git
 	}
 
 }
